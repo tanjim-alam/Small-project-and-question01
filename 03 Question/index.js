f0b0b76fdf4524dd8406eb5925381445444dd2c8
Tanjim Alam <122804862+Tanjim099@users.noreply.github.com>
2023-07-04 23:24:15 +0530

    Add files via upload

---
+++ 03 Question/index.js
@@ -0,0 +1,9 @@
+/*
+Execution context is an essential concept in JavaScript that defines the environment in which code is executed.
+It consists of various components that enable the interpretation and execution of JavaScript code. Understanding
+execution context is crucial for understanding how JavaScript code is executed.
+
+An execution context can be thought of as a wrapper that contains the code being executed
+and provides access to variables, functions, and other resources. Each time a function is called or
+code is executed globally, a new execution context is created.
+*/
